f9ec5d6da5e6511524b23dea49f2ac2c62b918b7
scribacious <keithlaidlaw@gmail.com>
2014-01-08 21:43:50 -0800

    more bug fixes

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function groupA()
 {
-
-    var A1 = "";
-    var A2 = "";
+    // var A1;
+    var A2;
 
     function getWinnerA()
     {
@@ -10,31 +9,27 @@ function groupA()
         {
         	if (document.getElementsByName('AW')[i].checked)
         	{
-        		A1 = (document.getElementsByName('AW')[i].value);
-    			break;
+        		var A1 = (document.getElementsByName('AW')[i].value);
+                //(document.getElementById('winnerA').innerHTML) = "Check 1";
+                break;
         	}
         } 
     };
+        alert(A1);
 
-
+    /*
     function getRunnerUpA()
     {
         for (var i = 0; i < 4; i++)
         {
             if (document.getElementsByName('ARU')[i].checked)
             {
-                A2 = (document.getElementsByName('ARU')[i].value);
+                // document.getElementById('runnerUpA').innerHTML = (document.getElementsByName('ARU')[i].value);
+                (document.getElementById('runnerUpA').innerHTML) = "Check 2";
                 break;
             }
         }
-    }; 
-
-    // alert("done2");
-
-
-    function returnGroupA(A1, A2)
-    {
-        alert(A1 + ", " + A2);
     };
+    */ 
 
- };
+};
